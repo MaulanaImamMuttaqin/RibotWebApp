460b643b64d06fcb101b2b8ace66bb7ebd808e45
=MaulanaImamMuttaqin <maulanaimammuttaqin2000@gmail.com>
2021-05-13 20:37:33 +0700

    tes2

--- src/components/Patient/Subcomponents/Detail.js
+++ src/components/Patient/Subcomponents/Detail.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect , useRef} from 'react'
 import { useParams } from 'react-router-dom'
 import firebase from "../../../firebase";
 import "./Detail.css"
@@ -12,12 +12,10 @@ export default function Detail() {
     
     const [patData, setPatData] = useState({})
     const [patRec, setPatRec] = useState([])
-    const [recModal, setRecModal] = useState(false)
+    const [newDoc, setNewDoc] = useState(false)
 
 
-    const openModal= ()=>{
-        setRecModal(!recModal)
-    }
+    
 
     useEffect(()=>{
         patient.doc(nik)
@@ -33,19 +31,46 @@ export default function Detail() {
             .onSnapshot(result =>{
                 const record = []
                 result.forEach(doc =>{
-                    record.push(doc.data())
+                    record.push([doc.data(), doc.id])
                 })
                 setPatRec(record)
             })
         
     },[])
 
-    console.log(patData)
-    console.log(patRec)
+    const sendNewRecordValue = (input) =>{
+        const {date, subject, description, drug, disease, docId} = input;
+        let timestamp = firebase.firestore.Timestamp.fromDate(new Date(date));
+        patient.doc(nik).collection("riwayatberobat").doc(docId).update({
+            record: firebase.firestore.FieldValue.arrayUnion({
+                conclusion: disease,
+                date: timestamp,
+                description: description,
+                subject:subject,
+                treatment: drug.replace(/\s/g, '').split(",")
+            })            
+        })
+        .then(() => {
+            console.log("success");
+        })
+        .catch((error) => {
+            console.error("failed: ", error);
+        });
+    }
+
+    const createNewDoctor = (input)=>{
+        patient.doc(nik).collection("riwayatberobat").add(input)
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+    }
 
     return (
         <div className="container">
-            {recModal && <AddModal/>}
+            {newDoc && <NewDoctorModal sendNewDocValue={createNewDoctor}/>}
             <div className="patient-detail">
                 <div><span>NAMA</span>   |  {patData.name}</div>
                 <div><span>NIK</span>   |  {patData.nik}</div>
@@ -56,10 +81,11 @@ export default function Detail() {
             <div className="patient-records">
                 <div className="title">
                     <h1>Riwayat Pasien</h1>
+                    <button onClick={()=> setNewDoc(!newDoc)}>Add New Doctor</button>
                 </div>
                 <div className="records">
-                    {patRec.map(rec =>
-                        <Records Data={rec} openModal={openModal}/>
+                    {patRec.map((rec, index) =>
+                        <Records key={index} Data={rec[0]} docId={rec[1]} sendNewRecordValue={sendNewRecordValue}/>
                     )}
                 </div>
             </div>
@@ -68,40 +94,93 @@ export default function Detail() {
 }
 
 
-function Records({Data, openModal}){
+function Records({Data, docId, sendNewRecordValue}){
     const [open, setOpen] = useState(false)
+    const [recModal, setRecModal] = useState(false)
 
-    
 
     return (
         <div className="record">
+            {recModal && <AddModal sendNewRecordValue={sendNewRecordValue} docId={docId}/>}
+
             <div className="doctor" onClick={()=> setOpen(!open)}>
                 <span>{Data.doctor.name}</span><span>{Data.doctor.speciality}</span>
             </div>
 
             <div className={`record-dates ${open && "open"}`}>
-                <button onClick={openModal}>add</button>
+                <button onClick={()=>setRecModal(!recModal)}>add</button>
                 
-                {Data.record.map(rec =>
-                    <Dates Record={rec}/>
+                {Data.record.map((rec, index) =>
+                    <Dates key={index} Record={rec} />
                     
                 )}
             </div>
         </div>
     )
 }
-function AddModal(){
+
+function NewDoctorModal({sendNewDocValue}){
+    const input ={
+        doctor:{},
+        record: []
+    }
+    
+
+    const inputValue = (e, field) => {
+        input.doctor[field] = e.target.value
+    }
+
+
     return(
         <div className="modal">
             <div>
-                <input type="date" />
-                <input type="text" name="" id="" placeholder="keluhan"/>
-                
+                <form onSubmit={e=>e.preventDefault()}>
+                    <label htmlFor="name">Nama</label>
+                        <input onChange={(e) => inputValue(e, "name")} id="name" className="addrecord" type="text" placeholder="nama" required/>
+                    <label htmlFor="speciality">Spesialis</label>
+                        <input onChange={(e) => inputValue(e, "speciality")} id="speciality" className="addrecord" type="text" placeholder="Jenis Doctor"required/>
+                    <button onClick={()=> {sendNewDocValue(input)}}>TAMBAH</button> 
+                </form>
             </div>
         </div>
     )
 }
-function Dates({Record}){
+
+function AddModal({sendNewRecordValue, docId}){
+    const input ={
+        docId: docId
+    }
+    
+
+    const inputValue = (e, name) => {
+        input[name] = e.target.value
+    }
+
+    
+
+    return(
+        <div className="modal">
+            <div>
+                <form onSubmit={e=> e.preventDefault()}>
+                    <label htmlFor="date">tanggal</label>
+                        <input onChange={(e) => inputValue(e, "date")} id="date" type="date" required/>
+                    <label htmlFor="subject">Subject</label>
+                        <input onChange={(e) => inputValue(e, "subject")} id="subject" className="addrecord" type="text" placeholder="subject"required/>
+                    <label htmlFor="description">Keterangan</label>
+                        <textarea onChange={(e) => inputValue(e, "description")} id="description" type="text" placeholder="Keterangan" rows="10" cols="50"required/>
+                    <label htmlFor="drug">Obat</label>
+                        <input onChange={(e) => inputValue(e, "drug")} id="drug" className="addrecord" type="text" placeholder="Obat yang diberikan, ex: nama_obat(dosis), namaobat2(dosis)"required/>
+                    <label htmlFor="disease">Penyakit</label>
+                        <input onChange={(e) => inputValue(e, "disease")} id="disease" className="addrecord"  type="text"  placeholder="Penyakit yang diduga"required/>  
+
+                    <button onClick={()=> sendNewRecordValue(input)}>TAMBAH</button> 
+                </form>
+            </div>
+        </div>
+    )
+}
+
+function Dates({Record}){   
     const [open, setOpen] =useState(false)
 
     const toogleDropDown = ()=>{
@@ -110,7 +189,7 @@ function Dates({Record}){
 
     return(
         <div>
-            <div className="date" onClick={(toogleDropDown)}>{Record.date.toDate().toDateString()}, ( {Record.conclusion} )</div>
+            <div className="date" onClick={(toogleDropDown)}>{Record.date.toDate().toDateString()}, ( {Record.subject} )</div>
             <div className={` description ${open && "open"}`}>
                 <div className="description-container">
                     <p>Keluhan</p>
@@ -118,7 +197,7 @@ function Dates({Record}){
                     <p>Pengobatan</p>
                     <ul>
                         {Record.treatment.map(drug=>
-                            <li>{drug}</li>
+                            <li key={drug}>{drug}</li>
                         )}
                         
                     </ul>
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/app"
 import "firebase/firestore"
 
-
+console.log(process.env.REACT_APP_FIREBASE_API_KEY)
 const firebaseConfig = {
     apiKey: "AIzaSyDhxARYdq6cVJ6cq-NHuOhT386RMnuRY3U",
     authDomain: "ninth-incentive-312907.firebaseapp.com",
@@ -16,3 +16,18 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig)
 
   export default firebase;
+
+  // const firebaseConfig = {
+  //   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  //   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  //   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  //   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  //   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  //   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
+  //   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  //   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  // };
+
+  // firebase.initializeApp(firebaseConfig)
+
+  // export default firebase;
